55c68d1219a391fbc1fb413ea669b289a46ad93f
VipulArora_2003 <99087370+starbanda@users.noreply.github.com>
2026-04-18 21:17:35 +0530

    Add: number-of-good-pairs - submission-0

---
+++ Data Structures & Algorithms/number-of-good-pairs/submission-0.java
@@ -0,0 +1,17 @@
+class Solution {
+    public int numIdenticalPairs(int[] nums) {
+        int i=0;
+        int count =0;
+        while (i<nums.length-1){
+        for(int j=i+1;j<nums.length;j++){
+            if(nums[i] == nums[j]) {
+                count ++;
+            }
+            
+
+        }
+        i++;
+    }
+    return count ;
+    }
+}
